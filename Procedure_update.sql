39f88659301580be1f31602ff5168c6ba8272638
Anandajayam <anandajayam@gmail.com>
2023-05-28 20:07:21 +0530

    add store pro

---
+++ Procedure_update.sql
@@ -0,0 +1,13 @@
+--test
+
+create or replace procedure money_tras(
+			sender int, reci int , amount dec) 
+as 
+$$
+	begin 
+		update t_account set balance=balance - amount where recid=sender;
+		update t_account set balance=balance + amount where recid=reci;
+	commit;
+	end;
+	
+$$ language plpgsql;
